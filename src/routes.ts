629e914ad24f692fde5ade8dc67f213459215206
Gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-06-10 09:03:03 -0300

    add editor config

--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,9 @@ import TranslateController from './controllers/TranslateController'
 
 const routes = express.Router(); 
 
-
 routes.get('/', TranslateController.index);
 // routes.get('/', (req, res) => {
 //   return res.json({msg: 'test'})
 // });
 
-
 export default routes;
-
-
