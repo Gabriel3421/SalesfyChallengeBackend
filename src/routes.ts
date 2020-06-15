7a68dc4ad5f7a291ea511c5f704b5513e1ee477b
Gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-06-15 11:03:02 -0300

    finalizing project

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,8 @@ import express from 'express';
 
 import TranslateController from './controllers/TranslateController'
 
-const routes = express.Router(); 
+const routes = express.Router();
 
 routes.get('/', TranslateController.index);
-// routes.get('/', (req, res) => {
-//   return res.json({msg: 'test'})
-// });
 
 export default routes;
--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -6,12 +6,10 @@ import app from '../src/app';
 const route = express.Router();
 
 describe("GET /", () => {
-  it('should always pass', function () {
-    expect(true).to.equal(true);
-  });
+
   it("should return an object with translated prop", async () => {
     const res = await request(app)
-        .get('/?translate=001');
+        .get('/?translate=1');
     expect(res.status).to.equal(200);
     expect(res.body).not.to.be.empty;
     expect(res.body).to.be.an("object");
